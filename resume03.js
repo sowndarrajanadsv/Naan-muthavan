074ad57d1987706e03472c8377bcd65c7f8690b0
Sowndarrajan A <145133786+sowndarrajanadsv@users.noreply.github.com>
2024-10-29 13:54:01 +0530

    Add files via upload

---
+++ resume03.js
@@ -0,0 +1,16 @@
+var sideo=document.querySelector(".nav-side")
+function show()
+{
+    sideo.style.left="0"
+}
+
+function closes()
+{
+    sideo.style.left="-60%"
+}
+
+
+
+
+// for(i=0;i<=10;i=i+1)
+//     console.log(i)
